5335c019da42b530ec5f3f6b0c9f7dfabff30167
unknown <arnavaggarwal999000@gmail.com>
2022-04-26 22:28:54 +0530

    making ui for homepage

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,15 +1,37 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch,Route} from "react-router-dom"
+import {BrowserRouter as Router, Switch,Route, Link} from "react-router-dom"
 import CreateRoomPage from './CreateRoomPage'
 import RoomJoinPage from './RoomJoinPage'
 import Room from "./Room"
+import { Grid, Typography, Button,ButtonGroup } from '@mui/material'
 
 function HomePage() {
+  function renderHomePage(){
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="primary" to="/create" component={Link}>
+              Create a room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    )
+  }
   return (
     <Router>
     <Switch>
       <Route exact path="/">
-        <p>This is the home page</p>
+        {renderHomePage()}
       </Route>
       <Route path="/join" component={RoomJoinPage} />
       <Route path="/create" component={CreateRoomPage} />
