e725e6e2b2216c89d9dc12c07725903ebd3d45d2
unknown <arnavaggarwal999000@gmail.com>
2022-04-28 21:48:25 +0530

    fixing frontend api bug

--- music-application/frontend/src/components/Room.js
+++ music-application/frontend/src/components/Room.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import {Grid, Button, Typography} from "@material-ui/core"
+import { Link } from "react-router-dom";
 
 export default class Room extends Component {
   constructor(props) {
@@ -10,11 +12,18 @@ export default class Room extends Component {
     };
     this.roomCode = this.props.match.params.roomCode;
     this.getRoomDetails();
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this)
   }
 
   getRoomDetails() {
     fetch("/api/get-room" + "?code=" + this.roomCode)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok){
+          this.props.leaveRoomCallBack()
+          this.props.history.push("/")
+        }
+        return         response.json()
+      })
       .then((data) => {
         this.setState({
           votesToSkip: data.votes_to_skip,
@@ -24,14 +33,44 @@ export default class Room extends Component {
       });
   }
 
+  leaveButtonPressed(){
+    const requestOptions = {
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+    }
+    fetch("/api/leave-room",requestOptions).then((response) => response.json()).then(
+      (data) => {
+        this.props.leaveRoomCallback()
+        this.props.history.push("/")
+      }
+    )
+  }
+
   render() {
+    console.log("Working")
     return (
-      <div>
-        <h3>{this.roomCode}</h3>
-        <p>Votes: {this.state.votesToSkip}</p>
-        <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-        <p>Host: {this.state.isHost.toString()}</p>
-      </div>
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Code: {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            guestCanPause: {this.guestCanPause}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            isHost: {this.isHost}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button onClick={this.leaveButtonPressed} variant="contained"color="secondary">
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
