4d9b5fbd289e743e56a4d2e6905cb88a7ead3fa3
unknown <arnavaggarwal999000@gmail.com>
2022-04-30 23:01:41 +0530

    creating util execute spotify api request

--- music-application/spotify/utils.py
+++ music-application/spotify/utils.py
@@ -5,6 +5,8 @@ from datetime import timedelta
 from .credentials import CLIENT_ID,CLIENT_SECRET
 import requests
 
+BASE_URL ="https://api.spotify.com/v1/me/"
+
 def get_user_tokens(session_id):
     user_tokens = SpotifyToken.objects.filter(user=session_id)
     if user_tokens.exists():
@@ -55,3 +57,19 @@ def is_spotify_authenticated(session_id):
         return True
     return False
 
+def execute_spotify_api_request(session_id,endpoint,post_=False,put_=False):
+    tokens = get_user_tokens(session_id)
+    header = {"Content-Type":"application/json","Authorization":"Bearer" + tokens.access_token}
+
+    if post_:
+        requests.post(BASE_URL + endpoint, headers=header)
+    if put_:
+        requests.put(BASE_URL + endpoint,headers=header)
+    
+    response = requests.get(BASE_URL + endpoint,{},headers=header)
+    try:
+        return response.json()
+    except:
+        return {"Error":"Issue with request"}
+
+    
--- music-application/spotify/views.py
+++ music-application/spotify/views.py
@@ -52,3 +52,11 @@ class IsAuthenticated(APIView):
     def get(self,request,format=None):
         is_authenticated  = is_spotify_authenticated(self.request.session.session_key)
         return Response({"status":is_authenticated},status=status.HTTP_200_OK)
+
+class CurrentSong(APIView):
+    def get(self,request,format=None):
+        room_code = self.request.session.get("room_code")
+        room = Room.objects.filter(code=room_code)[0]
+        host = room.host
+        endpoint = "/player/currently-playing"
+        
